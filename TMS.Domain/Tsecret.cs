853f3e0fb19ce3eb27f3182e1fd81b2a375c72bf
MdDilshadAliAnsari <104012788+MdDilshadAliAnsari@users.noreply.github.com>
2025-09-18 12:52:25 +0530

    Update Tsecret.cs
    CI:bA~8Q~iMNtwySZJ9jh.41nh-k2mu89GkHZRGLbUA
    DI:jst8Q~1PaAtq44.X3P9quEEVEoGOfJRMLHOSGaf-

--- TMS.Domain/Tsecret.cs
+++ TMS.Domain/Tsecret.cs
@@ -9,7 +9,21 @@ namespace TMS.Domain
     public  static class Tsecret
     {
      
+/// <summary>
+        /// This is Related COPTSupport ID
+        /// </summary>
+        //public static readonly string ClientId = "e98bf70a-9c3b-4c38-8580-4b7f936f1e6e";
+        //public static readonly string TenantId = "35d72887-7230-4bed-869f-58dce1433da2";
+        //public static readonly string ClientSecret = " ";
+        //public static readonly string Url = "https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messages?$filter=isRead eq false";
 
+        /// <summary>
+        /// This is Related Dilshad ID
+        /// </summary>
+        public static readonly string ClientId = "bf24f8a3-15c1-4a6c-acf7-e2ec22325ea8";
+        public static readonly string TenantId = "35d72887-7230-4bed-869f-58dce1433da2";
+        public static readonly string ClientSecret = " ";
+        public static readonly string Url = "https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messages?$filter=isRead eq false";
 
 
     }
